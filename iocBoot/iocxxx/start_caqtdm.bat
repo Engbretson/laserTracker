0fab6fe72969622503b60674f8d50a0d5d9cc4d1
Mark Engbretson <engbretson@anl.fov>
2021-08-03 12:56:58 -0500

    Add manual

--- iocBoot/iocxxx/start_caqtdm.bat
+++ iocBoot/iocxxx/start_caqtdm.bat
@@ -4,6 +4,6 @@ set CAQTDM_DISPLAY_PATH=C:\epics\LaserTracker-R1-0\xxxApp\op\ui\autoconvert
 set EPICS_CA_AUTO_ADDR_LIST=NO
 set EPICS_CA_ADDR_LIST=192.168.0.53
 
-caget 433LT:scope1:serialNumber_RBV
 
-call caqtdm -x -macro "P=433LT:, R=scope1:" LaserTracker.ui
+start caqtdm -x -macro "P=433LT:, R=scope1:" LaserTracker.ui
+call caqtdm -attach -x -macro "P=433LT:, R=scope1:" LT_measurement.ui 
