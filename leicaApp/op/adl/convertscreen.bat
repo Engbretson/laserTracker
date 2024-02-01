50798d4877bf36d361e39421bbebfe5319428903
mengbret <engbretson@anl.gov>
2024-02-01 12:23:36 -0600

    Mostly screen changes

--- leicaApp/op/adl/convertscreen.bat
+++ leicaApp/op/adl/convertscreen.bat
@@ -2,10 +2,10 @@
 for /r %%a in (*.adl) do "C:\Program Files\caQTDM\bin\windows-x64\adl2ui.exe" "%%a" 
 
 set OLDDIR=%CD%
-cd E:\phoebus-4.7.3
+cd d:\phoebus-4.7.3
 e:
 
-for  %%a in (C:\epics\areaDetector-R3-12-1\LaserTracker-R2-1\leicaApp\op\adl\*.adl) do call phoebus -main org.csstudio.display.converter.medm.Converter -force  "%%a" 
+for  %%a in (D:\epics\areaDetector-R3-12-1\LaserTracker-R2-1\leicaApp\op\adl\*.adl) do call phoebus -main org.csstudio.display.converter.medm.Converter -force  "%%a" 
 
 cd %OLDDIR%
 c:
